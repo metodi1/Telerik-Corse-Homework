bfdb6032d3b11dec6300cf70217024e89635a9bb
tddold <tdd@dekom.bg>
2014-08-27 14:36:24 +0300

    Update

---
+++ C# Part1/Primitive-Data-Types-and-Variables/10,MarketingFirmRecords/MarketingFirmRecords.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+//A marketing firm wants to keep record of its employees. Each record would have the following characteristics 
+//– first name, family name, age, gender (m or f), ID number, unique employee number (27560000 to 27569999). 
+//Declare the variables needed to keep the information for a single employee using appropriate data types and descriptive names.
+
+
+class MarketingFirmRecords
+{
+    static void Main()
+    {
+        Console.Title = "Marketing Firm Records";
+        Console.WriteLine(new string('-', 40) + "\nEnter your information:\n");
+        Console.Write("Enter your First Name:");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter your Last Name:");
+        string lastName = Console.ReadLine();
+        Console.Write("Enter your Age:");
+        byte age;
+        string gender;
+        string male = "male";
+        string female = "female";
+        int idNumber;
+        int uniqueEmployeeNumber;
+        do
+        {
+            age = byte.Parse(Console.ReadLine());
+            if ((age < 18) || (age > 65))
+            {
+                Console.WriteLine("The age you have entered is incorrect.");
+                Console.Write("Age:");
+            }
+        } while ((age < 18) || (age > 65));
+        Console.WriteLine("Enter Gender (male or female):");
+        do
+        {
+            gender = Console.ReadLine();
+            if ((gender != male) && (gender != female))
+            {
+                Console.WriteLine("The gender you have entered is incorrect.");
+                Console.Write("Gender:");
+            }
+        } while ((gender != male) && (gender != female));
+        Console.WriteLine("Enter ID number (101 to 1001):");
+        do
+        {
+            idNumber = int.Parse(Console.ReadLine());
+            if ((idNumber < 101) || (idNumber > 1001))
+            {
+                Console.WriteLine("The ID number you have entered is incorrect.");
+                Console.Write("ID number:");
+            }
+        } while ((idNumber < 101) || (idNumber > 1001));
+        Console.WriteLine("Enter Employee number (27560000 to 27569999):");
+        do
+        {
+            uniqueEmployeeNumber = int.Parse(Console.ReadLine());
+            if ((uniqueEmployeeNumber < 27560000) || (uniqueEmployeeNumber > 27569999))
+            {
+                Console.WriteLine("The Employee number you have entered is incorrect.");
+                Console.Write("ID number:");
+            }
+        } while ((uniqueEmployeeNumber < 27560000) || (uniqueEmployeeNumber > 27569999));
+
+        Console.WriteLine("\nYour name is: {0}\nYour Age is: {1}\nYour Gender is: {2}\nYour ID number is: {3}\nYour Employee number is: {4}\n", firstName + " " + lastName, age, gender, idNumber, uniqueEmployeeNumber);
+    }
+}
---
+++ C# Part1/Primitive-Data-Types-and-Variables/14.BankAccount/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//A bank account has a holder name (first name, middle name and last name), 
+//available amount of money (balance), bank name, IBAN, BIC code and 3 credit card numbers associated with the account. 
+//Declare the variables needed to keep the information for a single bank account 
+//using the appropriate data types and descriptive names.
+
+class BankAccount
+{
+    static void Main()
+    {
+        Console.Title = "Bank Account";
+        Console.Write("\nEnter First Name:");
+        string firstName = Console.ReadLine();
+        Console.Write("\nEnter Midle Name:");
+        string middleName = Console.ReadLine();
+        Console.Write("\nEnter Last Name:");
+        string lastName = Console.ReadLine();
+        Console.Write("\nEnter Balanse:");
+        decimal balance = decimal.Parse(Console.ReadLine());
+        Console.Write("\nEnter Bank Name:");
+        string bankName = Console.ReadLine();
+        Console.Write("\nEnter IBAN:");
+        string iban = Console.ReadLine();
+        Console.Write("\nEnter BIC:");
+        string bic = Console.ReadLine();
+        Console.Write("\nEnter Number of Credit Cart One:");
+        ulong numberFirstCreditCart = ulong.Parse(Console.ReadLine());
+        Console.Write("\nEnter Number of Credit Cart One:");
+        ulong numberSecondCreditCart = ulong.Parse(Console.ReadLine());
+        Console.Write("\nEnter Number of Credit Cart One:");
+        ulong numberThirdCreditCart = ulong.Parse(Console.ReadLine());
+        Console.WriteLine("\nName: {0}\nBalanse: {1}\nBank: {2}\nIBAN: {3}\nBIC: {4}\nID First Cart: {5}\nID Second Cart: {6}\nID Third Cart: {7}\n", firstName + " " + middleName + " " + lastName, balance, bankName, iban, bic, numberFirstCreditCart, numberSecondCreditCart, numberThirdCreditCart);
+
+    }
+}
