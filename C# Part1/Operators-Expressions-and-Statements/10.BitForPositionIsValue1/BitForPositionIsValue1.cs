2810905a2b64757ba496513fb6f111859fc25b1e
tddold <tdd@dekom.bg>
2014-08-27 14:25:52 +0300

    Update

---
+++ C# Part1/Operators-Expressions-and-Statements/10.BitForPositionIsValue1/BitForPositionIsValue1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Write a boolean expression that returns if the bit at position p (counting from 0) in a given integer number v has value of 1. Example: v=5; p=1  false.
+
+class BitForPositionIsValue1
+{
+    static void Main()
+    {
+        Console.Title = "Check Bit For X Position is Vlue 1";
+        int checkNumber = int.Parse(Console.ReadLine());
+        int checkPosition = int.Parse(Console.ReadLine());
+        int mask = 1 << checkPosition;
+        int numberAndMask = checkNumber & mask;
+        int result = numberAndMask >> checkPosition;
+        if (result == 1)
+        {
+            Console.WriteLine("Result is True");
+        }
+        else
+            Console.WriteLine("Result is False");
+    }
+}
---
+++ C# Part1/Operators-Expressions-and-Statements/11.ValueOfBitAtPosition/ValueOfBitAtPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Write an expression that extracts from a given integer i the value of a given bit number b. Example: i=5; b=2  value=1.
+
+class ValueOfBitAtPosition
+{
+    static void Main()
+    {
+        Console.Title = "Check Bit For X Position is Vlue 1";
+        int checkNumber = int.Parse(Console.ReadLine());
+        int checkPosition = int.Parse(Console.ReadLine());
+        int mask = 1 << checkPosition;
+        int numberAndMask = checkNumber & mask;
+        int result = numberAndMask >> checkPosition;
+        Console.WriteLine("The bit of position is: {0}", result);
+    }
+}
