f95270562c6944f85af2d1672124015c8ea146fd
tddold <tdd@dekom.bg>
2014-08-27 14:31:02 +0300

    Update

---
+++ C# Part1/Primitive-Data-Types-and-Variables/01.PrintFiveVariables/FiveVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class FiveVariables
+{
+    static void Main()
+    {
+        Console.Title = "Print-Five-Variables";
+        Console.WriteLine(new string('-', 40) + "\n Example for five variables.\n");
+        //52130, -115, 4825932, 97, -10000
+        byte firstVariable = 97;
+        sbyte secondVariable = -115;
+        ushort thirdVariables = 52130;
+        short forVariable = -10000;
+        int fiveVeriable = 4825932;
+        Console.WriteLine(new string('-', 40));
+        Console.WriteLine(" First variable\t is \"byte\"\t = {0}\n Second variable is \"sbyte\"\t = {1}\n Third variable\t is \"ushort\"\t = {2}\n For variable\t is \"short\"\t = {3}\n Five variable\t is \"int\"\t = {4}\n", firstVariable, secondVariable, thirdVariables, forVariable, fiveVeriable);
+        Console.WriteLine(new string('-', 40));
+    }
+}
---
+++ C# Part1/Primitive-Data-Types-and-Variables/02.FloatOrDouble/FloatOrDouble.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//Which of the following values can be assigned to a variable of type
+//float and which to a variable of type double: 34.567839023, 12.345, 8923.1234857, 3456.091?
+
+class FloatOrDouble
+{
+    static void Main()
+    {
+        Console.Title = "Print Float Or Double Type";
+        Console.WriteLine(new string('-',40) + "\nExamle for Float or Double Type Variables\n");
+        float firstFloatVariable = 12.345f;
+        float secondFloatVariable = 3456.091f;
+        double firstDoubleVariable = 34.567839023;
+        double secondDoubleVariable = 8923.1234857;
+        Console.WriteLine(new string('-',40));
+        Console.WriteLine("\nThe Floating Type Variables is:\t {0},\t {1}", firstFloatVariable, secondFloatVariable);
+        Console.WriteLine("\nThe Double Type Variables is:\t {0},\t {1}", firstDoubleVariable, secondDoubleVariable);
+        Console.WriteLine(new string('-', 40));
+        //float pi = 13.141592653589793238f;
+        //Console.WriteLine("pi = {0}", pi);
+    }
+}
