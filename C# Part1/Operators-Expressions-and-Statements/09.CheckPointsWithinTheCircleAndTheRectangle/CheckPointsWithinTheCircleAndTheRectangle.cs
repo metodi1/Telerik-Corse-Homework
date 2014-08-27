61abb8f51534d093a4f7f9bf5dfd5ec56c837d47
tddold <tdd@dekom.bg>
2014-08-27 14:30:11 +0300

    Update

---
+++ C# Part1/Operators-Expressions-and-Statements/09.CheckPointsWithinTheCircleAndTheRectangle/CheckPointsWithinTheCircleAndTheRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Write an expression that checks for given point (x, y) if it is within the circle K( (1,1), 3) and out of the rectangle R(top=1, left=-1, width=6, height=2).
+
+class CheckPointsWithinTheCircleAndTheRectangle
+{
+    static void Main()
+    {
+        Console.Title = "Check Points X and Y";
+        int centerCircleX = 1;
+        int centerCircleY = 1;
+        int radius = 3;
+        int pointX = int.Parse(Console.ReadLine());
+        int pointY = int.Parse(Console.ReadLine());
+        if ((((pointX - centerCircleX) * (pointX - centerCircleX)) + ((pointY - centerCircleY) * (pointY - centerCircleY)) <= radius * radius) && pointX >= -1 && pointX <= 5 && pointY >= -1 && pointY <= 1)
+        {
+            Console.WriteLine("The point (x, y) is in");
+        }
+        else
+            Console.WriteLine("The point (x, y) isn't in");
+    }
+}
---
+++ C# Part1/Operators-Expressions-and-Statements/12.Test/FreeBitsExchenges.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+//* Write a program that exchanges bits {p, p+1, …, p+k-1) with bits {q, q+1, …, q+k-1} of given 32-bit unsigned integer.
+
+class FreeBitsExchenges
+{
+    static void Main()
+    {
+        Console.Title = "Bit Exchenge";
+        Console.Write("n:");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("p:");
+        int p = int.Parse(Console.ReadLine());
+        Console.Write("q:");
+        int q = int.Parse(Console.ReadLine());
+        Console.Write("k:");
+        int k = int.Parse(Console.ReadLine());
+        int maskp = 1 << p;
+        int maskq = 1 << q;
+        int resultLast;
+        Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+        for (int i = 0; i < k; i++)
+        {
+            maskp = 1 << p + i;
+            maskq = 1 << q + i;
+            int maskqInvert = ~maskq;
+            Console.WriteLine(Convert.ToString(maskp, 2).PadLeft(32, '0'));
+            Console.WriteLine(Convert.ToString(maskq, 2).PadLeft(32, '0'));
+            int nAndMaskp = n & maskp;
+            int nAndMaskq = n & maskq;
+            //int nAndMaskqInvert = ~nAndMaskq;
+            Console.WriteLine(Convert.ToString(nAndMaskp, 2).PadLeft(32, '0'));
+            Console.WriteLine(Convert.ToString(nAndMaskq, 2).PadLeft(32, '0'));
+            int result;
+            if (nAndMaskq == 0)
+            {
+                result = n & nAndMaskq >> (maskq - maskp);
+            }
+            else
+            {
+                result = n | (maskq - maskp);
+            }
+            int[] resultNew = { result };
+            int z = i - 1;
+            if (z<=0)
+            {
+                z = 0;
+            }
+
+            Console.WriteLine(z);
+            Console.WriteLine(resultNew);
+            if (nAndMaskq == 0)
+            {
+                resultLast = resultNew[z] & resultNew[i];
+            }
+            else
+            {
+                resultLast = resultNew[z] | resultNew[2];
+            }
+            Console.WriteLine(Convert.ToString(resultLast, 2).PadLeft(32, '0'));
+            Console.WriteLine(resultLast);
+        }
+    }
+}
