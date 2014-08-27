50256daaf2ab62856d6d66e529d27acd973bf904
tddold <tdd@dekom.bg>
2014-08-27 14:24:17 +0300

    Update

---
+++ C# Part1/Operators-Expressions-and-Statements/06.ChekXYPositionOnCircle/ChekXYPositionOnCircle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+////Write an expression that checks if given point (x,  y) is within a circle K(O, 5).
+
+class ChekXYPositionOnCircle
+{
+    static void Main(string[] args)
+    {
+        //Console.Title = "Chek position X and Y";
+        //int x = 0;
+        //int y = 5;
+        //int inputX = int.Parse(Console.ReadLine());
+        //int inputY = int.Parse(Console.ReadLine());
+        //if (inputX <= y && inputY <= y)
+        //{
+        //    Console.WriteLine("X and Y is in circle");
+        //}
+        //else
+        //    Console.WriteLine("X and Y is dont in circle");
+
+        Console.Title = "Chek position X and Y";
+        int x = 0;
+        int radius = 5;
+        int inputX = int.Parse(Console.ReadLine());
+        int inputY = int.Parse(Console.ReadLine());
+        if (inputX*inputX + inputY*inputY <= radius*radius)
+        {
+            Console.WriteLine("X and Y is in circle");
+        }
+        else
+            Console.WriteLine("X and Y is dont in circle");
+    }
+}
