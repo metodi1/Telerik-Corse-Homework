a05aa38bb1435d6d57efd7f8e1532f91ab03c7bf
tddold <tdd@dekom.bg>
2014-08-27 14:31:43 +0300

    Update

---
+++ C# Part1/Primitive-Data-Types-and-Variables/04.HexadecimalFormat/HexadecimalFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//Declare an integer variable and assign it with the value 254 in hexadecimal format.
+//Use Windows Calculator to find its hexadecimal representation.
+
+
+class HexadecimalFormat
+{
+    static void Main()
+    {
+        Console.Title = "Hexadecimal Format";
+        byte number = 0xfe;
+        Console.WriteLine(number);
+    }
+}
