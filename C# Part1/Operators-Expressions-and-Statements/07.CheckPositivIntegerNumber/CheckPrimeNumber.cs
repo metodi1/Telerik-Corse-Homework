57fdc4aa8b8ba266e53a55289221550079aa783e
tddold <tdd@dekom.bg>
2014-08-27 14:24:57 +0300

    Update

---
+++ C# Part1/Operators-Expressions-and-Statements/07.CheckPositivIntegerNumber/CheckPrimeNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//Write an expression that checks if given positive integer number n (n ≤ 100) is prime. E.g. 37 is prime.
+
+class CheckPrimeNumber
+{
+    static void Main()
+    {
+        Console.Title = "Check The Number Is Prime";
+        int integerNumber = int.Parse(Console.ReadLine());
+        int n = integerNumber;
+        if (n <= 100)
+        {
+            if (((n % 2 != 0) && (n % 3 != 0) && (n % 5 != 0) && (n % 7 != 0) && (n % 9 != 0)) || (n == 2) || (n == 3) || (n == 5) || (n == 7) || (n == 9))
+            {
+                Console.WriteLine("The number is prime");
+            }
+            else
+                Console.WriteLine("The number isn't prime");
+        }
+    }
+}
---
+++ C# Part1/Operators-Expressions-and-Statements/08.CalculateTrapezoidArea/CalculateTrapezoidArea .cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Write an expression that calculates trapezoid's area by given sides a and b and height h.
+
+    class CalculateTrapezoidArea 
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calculate Trapezoid Area";
+            int sidesA = int.Parse(Console.ReadLine());
+            int sidesB = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int area = (sidesA + sidesB) * height / 2;
+            Console.WriteLine("The trapezoid area is: " + area);
+        }
+    }
