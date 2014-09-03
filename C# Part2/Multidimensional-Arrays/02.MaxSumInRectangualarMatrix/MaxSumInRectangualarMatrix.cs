73d1985e9a99bfebad05d611965d72c48deefba3
tddold <tdd@dekom.bg>
2014-09-03 18:46:19 +0300

    Update

--- C# Part2/Multidimensional-Arrays/02.MaxSumInRectangualarMatrix/MaxSumInRectangualarMatrix.cs
+++ C# Part2/Multidimensional-Arrays/02.MaxSumInRectangualarMatrix/MaxSumInRectangualarMatrix.cs
@@ -41,9 +41,9 @@ class MaxSumInRectangualarMatrix
         //}
 
         int[,] matrix = {
-  {7, 1, 3, 3, 2, 1},
-  {1, 3, 9, 8, 5, 6},
-  {4, 6, 7, 9, 1, 0} };
+          {7, 1, 3, 3, 2, 1},
+          {1, 3, 9, 8, 5, 6},
+          {4, 6, 7, 9, 1, 0} };
 
 
         for (int i = 0; i < matrix.GetLength(0) - (platformMaxX - 1); i++)
