a99c1219eabc20f22e80f4e08d77b67976b536e5
Todor Dakov <tdd@dekom.bg>
2014-09-05 02:42:52 +0300

    Update

---
+++ C# Part2/Multidimensional-Arrays/04.ArrayBinSearch/ArrayBinSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Write a program, that reads from the console an array of N integers and an integer K, sorts the array and using the method Array.BinSearch() finds the largest number in the array which is ≤ K.
+
+class ArrayBinSearch
+{
+    //static void GetArray()
+    //{
+    //    List<string> list = new List<string>();
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        list.Add()
+    //    }
+    //}
+    static void PrintArray(int[] array)
+    {
+        int size = (int)Math.Log10(array.Length);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(Convert.ToString(array[i]).PadRight(size, ' '));
+        }
+    }
+
+    static void Main()
+    {
+        ////input
+        int N = 10;
+        Random rdn = new Random();
+        
+         int[] array = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            array[i] = rdn.Next();
+        }
+
+        foreach (var item in array)
+        {
+            Console.Write(Convert.ToString(array.Length).PadRight(3, ' '));
+        }
+        
+
+        PrintArray(array);
+    }
+}
