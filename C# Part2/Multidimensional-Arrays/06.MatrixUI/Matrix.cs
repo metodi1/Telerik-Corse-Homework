8e038ee9ee7af2dceda4ec2f8df70fb274df752e
tddold <tdd@dekom.bg>
2014-09-11 19:49:11 +0300

    Update

---
+++ C# Part2/Multidimensional-Arrays/06.MatrixUI/Matrix.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Matrix
+{
+    private int[,] matrix;
+
+    public Matrix(int rows, int cols)
+    {
+        this.matrix = new int[rows, cols];
+    }
+
+    public int Rows
+    {
+        get
+        {
+            return this.matrix.GetLength(0);
+        }
+
+    }
+
+    public int Colums
+    {
+        get
+        {
+            return this.matrix.GetLength(1);
+        }
+    }
+
+    public static Matrix operator +(Matrix first, Matrix second)
+    {
+        Matrix result = new Matrix();
+        for (int row = 0; row < first.Rows; row++)
+        {
+            for (int col = 0; col < first.Colums; col++)
+            {
+                result[row, col] = first[row, col] + second[row, col];
+            }
+        }
+
+        return result;
+    }
+
+    public int this[int row, int col]
+    {
+        get
+        {
+            return this.matrix[row, col];
+        }
+
+        set
+        {
+            this.matrix[row, col] = value;
+        }
+    }
+}
---
+++ C# Part2/Multidimensional-Arrays/06.MatrixUI/MatrixUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MatrixUI
+{
+    static void Main()
+    {
+        Matrix matrixFirst = new Matrix(2.,2);
+
+        Matrix matrixSecond = new Matrix(2,2);
+
+    }
+}
