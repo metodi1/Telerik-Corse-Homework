4ee485e54e4339d58a3470ba7ecbe17787d766cf
tddold <tdd@dekom.bg>
2014-09-13 16:38:37 +0300

    Update

---
+++ C# Part2/Methods/Test/Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Test
+{
+    static void PrintSign(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine("The number {0} is positive", number);
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number {0} is negativ", number);
+        }
+        else
+        {
+            Console.WriteLine("The number {0} is zero", number);
+        }
+    }
+
+    static void PrintMax(float number1, float number2)
+    {
+        float max = number1;
+        if (number2 > number1)
+        {
+            max = number2;
+        }
+
+        Console.WriteLine("Maximal number: {0}", max);
+    }
+
+    static void SayMonth(int month)
+    {
+        string[] monthNames = new string[] {
+            "Jenuary", "February", "March", "April", "May", "June", "July", "August", "September", "Octouber", "November", "December"};
+        Console.WriteLine(monthNames[month -1]);
+    }
+
+    static void SayPeriod(int startMonth, int endMonth)
+    {
+        int period = endMonth - startMonth;
+        if (period<0)
+        {
+            period = period + 12;
+        }
+
+        Console.Write("There are {0} months from ", period);
+        SayMonth(startMonth);
+        Console.Write(" to ");
+        SayMonth(endMonth);
+    }
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("For period enter start month: ");
+        int startMonth = int.Parse(Console.ReadLine());
+        Console.Write("For period enter end month: ");
+        int endMonth = int.Parse(Console.ReadLine());
+        PrintSign(number);
+        PrintMax(5, 2);
+        PrintMax(-5, 2);
+        SayMonth(9);
+        SayPeriod(startMonth, endMonth);
+    }
+}
---
+++ C# Part2/Methods/Test2/Test2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Test2
+{
+    static void PrintLine(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(" {0}", i);
+        }
+
+        Console.WriteLine();
+    }
+    static void Main()
+    {
+        Console.Write("Enter number n:");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int line = 1; line <= n; line++)
+        {
+            PrintLine(1, line);
+        }
+
+        for (int line = n-1; line >= 1; line--)
+        {
+            PrintLine(1, line);
+        }
+    }
+}
