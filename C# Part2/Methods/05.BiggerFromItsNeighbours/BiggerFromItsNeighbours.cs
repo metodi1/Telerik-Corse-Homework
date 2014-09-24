e1251ce4f54d10bbb7033908ec753e0f6fa4a92b
Todor Dakov <tdd@dekom.bg>
2014-09-25 01:00:28 +0300

    Update

---
+++ C# Part2/Methods/05.BiggerFromItsNeighbours/BiggerFromItsNeighbours.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/*Write a method that checks if the element at given position in given array of integers is bigger than its two neighbors (when such exist).
+*/
+
+
+class BiggerFromItsNeighbours
+{
+    public static bool BiggerThanItsNeghbours(int[] array, int position)
+    {
+        
+        if (true)
+        {
+            
+        }
+        return true;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter positon number: ");
+        int position = int.Parse(Console.ReadLine());
+        int[] array = { 1, 4, 9, 3, 7, 8, 2, 6 };
+    }
+}
