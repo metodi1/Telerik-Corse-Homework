0eae7049f1a6ff70331026cc1f047e8b4a72a6bd
tddold <tdd@dekom.bg>
2014-10-24 18:52:18 +0300

    Update

--- C# Part2/04. Numeral Systems/Test/Test.cs
+++ C# Part2/04. Numeral Systems/Test/Test.cs
@@ -7,17 +7,35 @@ namespace _01.Decimal_to_Binary
         static void Main(string[] args)
         {
             Console.Write("Decimal: ");
-            int decimalNumber = int.Parse(Console.ReadLine());
+            //int decimalNumber = int.Parse(Console.ReadLine());
 
-            int remainder;
-            string result = string.Empty;
-            while (decimalNumber > 0)
+            //int remainder;
+            //string result = string.Empty;
+            //while (decimalNumber > 0)
+            //{
+            //    remainder = decimalNumber % 2;
+            //    decimalNumber /= 2;
+            //    result = remainder.ToString() + result;
+            //}
+            //Console.WriteLine("Binary:  {0}", result);
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= number; i++)
             {
-                remainder = decimalNumber % 2;
-                decimalNumber /= 2;
-                result = remainder.ToString() + result;
+                int tmp = i;
+                int spasceNum = number-i;
+
+                string star = new string('*', i);
+                string space = new string(' ', spasceNum);
+                char tree = '|';
+                Console.Write(space);
+                Console.Write(star);
+                Console.Write(tree);
+                Console.Write(star);
+                Console.Write(space);
+                Console.WriteLine();
             }
-            Console.WriteLine("Binary:  {0}", result);
         }
     }
 }
