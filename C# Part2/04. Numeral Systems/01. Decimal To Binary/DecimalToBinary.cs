3a11ad4c7201a5147157df5125ce8690b6c4f114
Todor Dakov <tdd@dekom.bg>
2014-11-02 19:13:06 +0200

    Update

--- C# Part2/04. Numeral Systems/01. Decimal To Binary/DecimalToBinary.cs
+++ C# Part2/04. Numeral Systems/01. Decimal To Binary/DecimalToBinary.cs
@@ -59,6 +59,7 @@ class DecimalToBinary
     static void Main()
     {
         Console.Write("Enter number: ");
+        
         int number = int.Parse(Console.ReadLine());
 
         Console.WriteLine(GetBinary(number));
