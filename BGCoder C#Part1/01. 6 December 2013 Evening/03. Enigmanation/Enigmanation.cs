b228d5f57026b1174cf256714adad15aaddc3af8
Todor Dakov <tdd@dekom.bg>
2014-12-08 01:24:14 +0200

    Update

---
+++ BGCoder C#Part1/01. 6 December 2013 Evening/03. Enigmanation/Enigmanation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Enigmanation
+{
+    static void Main()
+    {
+        string expression = "(1+9)";
+        int position = 0;
+        int subExpressResult = 0;
+
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {0}", '(', ')', '%', '*', '-', '+');
+
+        foreach (char n in expression)
+        {
+            if ((char)n >=48 && (char)n <= 57)
+            {
+                
+            }
+        }
+    }
+}
