9fcb3a46abd557a64f8bd3007417199ca5af215a
Todor Dakov <tdd@dekom.bg>
2015-01-07 03:45:22 +0200

    Update

--- BGCoder C#Part1/10. 5 December 2013 Evening/NaBabaMiSmetalnika/NaBabaMiSmetalnika.cs
+++ BGCoder C#Part1/10. 5 December 2013 Evening/NaBabaMiSmetalnika/NaBabaMiSmetalnika.cs
@@ -27,10 +27,7 @@ class NaBabaMiSmetalnika
         string comand = String.Empty;
         int row = 0;
         int col = 0;
-
-        int cheskBitOne = 0;
-        int mask = 0;
-        int bit = 0;
+       
         while (comand != "stop")
         {
             comand = Console.ReadLine();
@@ -39,24 +36,18 @@ class NaBabaMiSmetalnika
             {
                 row = int.Parse(Console.ReadLine());
                 col = int.Parse(Console.ReadLine());
-              
-                cheskBitOne = 0;
-                mask = 0;
-                bit = 0;
+
                 for (int i = 0; i <= col; i++)
                 {
                     if (matrix[row, i] == 1 && i > 0)
                     {
                         for (int j = 0; j < i; j++)
                         {
-                            mask = 1 << j;
-                            cheskBitOne = matrix[row, j] & mask;
-                            bit = cheskBitOne >> j;
-                            if (bit == 0)
+
+                            if (matrix[row, j] == 0 && matrix[row, i] == 1)
                             {
                                 matrix[row, j] = 1;
                                 matrix[row, i] = 0;
-                                mask = 0;
                                 i++;
                                 if (i >= col)
                                 {
@@ -73,24 +64,17 @@ class NaBabaMiSmetalnika
             {
                 row = int.Parse(Console.ReadLine());
                 col = int.Parse(Console.ReadLine());
-
-                cheskBitOne = 0;
-                mask = 0;
-                bit = 0;
-                for (int i = 0; i <= col; i++)
+                                
+                for (int i = n - col; i>=0; i--)
                 {
                     if (matrix[row, i] == 1 && i < n-1)
                     {
-                        for (int j = 0; j <i; j++)
-                        {
-                            mask = 1 << j;
-                            cheskBitOne = matrix[row, j] & mask;
-                            bit = cheskBitOne >> j;
-                            if (bit == 0)
+                        for (int j = n-1; j > i; j--)
+                        {                            
+                            if (matrix[row, j] == 0 && matrix[row, i] ==1)
                             {
                                 matrix[row, j] = 1;
                                 matrix[row, i] = 0;
-                                mask = 0;
                                 i++;
                                 if (i >= col)
                                 {
@@ -102,26 +86,86 @@ class NaBabaMiSmetalnika
                 }
 
             }
-        }
 
-        //print
-        for (int row1 = 0; row1 < matrix.GetLength(0); row1++)
-        {
-            for (int col1 = matrix.GetLength(1) - 1; col1 >= 0; col1--)
+            if (comand == "reset")
             {
-                if (matrix[row1, col1] == 0)
+                row = 0;
+                col = 0;
+                for (int m = row; m < n; m++)
                 {
-                    Console.Write('.');
+                    for (int i = n - 1; i >= 0; i--)
+                    {
+                        if (matrix[m, i] == 1 && i < n - 1)
+                        {
+                            for (int j = n - 1; j > 0; j--)
+                            {
+                                if (matrix[m, j] == 0 && matrix[m, i] == 1)
+                                {
+                                    matrix[m, j] = 1;
+                                    matrix[m, i] = 0;
+                                    i++;
+                                    if (i >= col)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
-                else if (matrix[row1, col1] == 1)
-                {
-                    Console.Write('*');
-                }
+                
 
             }
-
-            Console.WriteLine();
         }
 
+        ////print
+        //for (int row1 = 0; row1 < matrix.GetLength(0); row1++)
+        //{
+        //    for (int col1 = matrix.GetLength(1) - 1; col1 >= 0; col1--)
+        //    {
+        //        if (matrix[row1, col1] == 0)
+        //        {
+        //            Console.Write('.');
+        //        }
+        //        else if (matrix[row1, col1] == 1)
+        //        {
+        //            Console.Write('*');
+        //        }
+
+        //    }
+
+        //    Console.WriteLine();
+        //}
+
+        // result
+        int result = 0;
+        for (int row2 = 0; row2 < matrix.GetLength(0); row2++)
+        {
+            for (int col2 = 0; col2 < matrix.GetLength(1); col2++)
+            {
+                result += matrix[row2, col2] * (int) Math.Pow(2, col2);
+            }
+            // Console.WriteLine(result);
+        }
+
+        int count = 0;
+        for (int col3 = 0; col3 < matrix.GetLength(1); col3++)
+        {
+            int tempCount = 0;
+            for (int row3 = 0; row3 < 8; row3++)
+            {
+                if (matrix[row3, col3] == 0)
+                {
+                    tempCount++;
+                }
+            }
+
+            if (tempCount == 8)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(result * count);
     }
 }
